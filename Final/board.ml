057d60c099aef785913561200d2c59523ffa21f9
June Kim <jooeun@caltech.edu>
2019-03-20 05:03:39 -0700

    finished part b

--- Final/board.ml
+++ Final/board.ml
@@ -70,6 +70,20 @@ module Make (S: Storage) : Board =
 	  let possible_moves = 
 	    [(-2, -1); (-1, -2); (1, -2); (2, -1); (2, 1); (1, 2); (-1, 2);
 	      (-2, 1)] 
+        
+    (* Get all possible locs a knight's move away on board from the
+     * given loc (r, c) *)
+    let get_next_locs board (r, c) =
+      let rec iter moves ans =
+        match moves with
+          | [] -> ans
+          | (r', c') :: t ->
+            let loc = (r + r', c + c') in
+              if check_bounds board loc
+                then iter t (loc :: ans)
+                else iter t ans (* Discard if off board *)
+      in
+        iter possible_moves []
 	
     let init_reachable nrows ncols =
       (* Helper function to count reachability from (r, c) *)
@@ -133,30 +147,96 @@ module Make (S: Storage) : Board =
       if not(check_bounds board loc)
         then bad_loc "get_loc_counts_from_loc" "location off board" loc
         else
+          (* Iterate through all locs a Knight's move away on board and 
+           * add (loc, reachability) if loc is unoccupied *)
           let rec iter locs ans =
             match locs with
               | [] -> ans
               | h :: t ->
-                try
-                  match get_reachable board h with
+                match get_reachable board h with
                     | None -> iter t ans (* Discard if null loc *)
                     | Some i -> iter t ((h, i) :: ans)
-                with
-                  (* Discard if loc off board *)
-                  Invalid_argument _ -> iter t ans
           in
-          let (r, c) = loc in
-          let next_locs = 
-            List.map (fun (r', c') -> (r + r', c + c')) possible_moves
-          in
-            iter next_locs [] 
+            iter (get_next_locs board loc) [] 
     
-    let place board loc = 
-      failwith "TODO"
+    let place board loc =
+      if not(check_bounds board loc) then
+        bad_loc "place" "location off board" loc
+      else if get_index board loc <> None then
+        bad_loc "place" "location is already occupied" loc
+      else
+        (* Helper function that returns true if it a knight's move away
+         * form the last placed knight, false otherwise *)
+        let is_knights_move (r, c) = 
+          let (r', c') = get_last board in
+          let (r_dist, c_dist) = (abs (r - r'), abs (c - c')) in
+            (r_dist = 2 && c_dist = 1) || (r_dist = 1 && c_dist = 2)
+        in
+          if board.last_index <> 0 && not (is_knights_move loc)
+            then bad_loc "place" "loc is not a knight's move" loc
+      else (* If the placement is valid *)
+        (* Update placed list *)
+        let placed = loc :: board.placed in
+        (* Update last index *)
+        let last_index = board.last_index + 1 in
+        (* Update indices grid *)
+        let indices = S.set board.indices loc last_index in
+        (* Update reachable grid *)
+        let rec iter locs grid = 
+          match locs with
+            | [] -> grid
+            | h :: t ->
+              (* Decrement reachability by 1 if not null *)
+              match get_reachable board h with
+                | None -> iter t grid
+                | Some i -> iter t (S.set grid h (i - 1)) 
+        in
+        (* Set loc to null in reachable and update all non-null locs
+         * a Knight's move away in reachable *)
+        let reachable = 
+          iter (get_next_locs board loc) (S.remove board.reachable loc)
+        in
+          { board with placed; last_index; indices; reachable }
+        
 
     let undo board = 
-      failwith "TODO"
-
+      (* Return the input board if there was no last move *)
+      if board.last_index = 0 then
+        board
+      else
+        let last_loc = List.hd board.placed in
+        (* Update placed list *)
+        let placed = List.tl board.placed in
+        (* Update last index *)
+        let last_index = board.last_index - 1 in
+        (* Update indices grid *)
+        let indices = S.remove board.indices last_loc in
+        (* Update reachable grid *)
+        let rec iter locs grid = 
+          match locs with
+            | [] -> grid
+            | h :: t ->
+              (* Increment reachability by 1 if not null *)
+              match get_reachable board h with
+                | None -> iter t grid
+                | Some i -> iter t (S.set grid h (i + 1)) 
+        in
+        (* Helper function to recompute reachability of loc *)
+        let rec count_reachability moves count =
+          match moves with
+            | [] -> count
+            | h :: t -> 
+              let cur = if get_index board h = None then 1 else 0 in
+                count_reachability t (count + cur)
+        in
+        (* Update reachability of loc and update all non-null locs a
+         * Knight's move away in reachable *)
+        let next_locs = get_next_locs board last_loc in
+        let count = count_reachability next_locs 0 in
+        let reachable' = S.set board.reachable last_loc count in
+        let reachable = iter next_locs reachable' in
+          { board with placed; last_index; indices; reachable }
+          
     let is_solved board = board.last_index = board.size
     let get_placed board = List.rev board.placed
   end
